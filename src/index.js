03adbf2d3d5d700354e8123e60f895ee9102c35b
Michael Brooks <me@michaelbrooks.co.uk>
2017-02-10 14:33:21 +0000

    Added routing and modal for selected blog post

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
-import App from './App';
+import App from './Components/App';
+import Post from './Components/Post';
 
 import 'bulma/css/bulma.css'
 import './index.css';
 
 ReactDOM.render(
-    <App />,
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App }>
+        <Route path="/post/:slug" component={ Post } />
+      </Route>
+    </Router>,
   document.getElementById('root')
 );
